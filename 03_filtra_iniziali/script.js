3e6f2a5f65cdfeab4eb86bbe51c0e2aa234f92bf
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-02 00:14:59 +0200

    risoluzione 03_filtra_iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filterByLetter(array, letter) {
+    const result = [];
 
+    for (let i = 0; i < array.length; i++) {
+        if (array[i][0] === letter) {
+            result.push(array[i]);
+        }
+    }
+
+    return result;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(filterByLetter(names, "A"));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
