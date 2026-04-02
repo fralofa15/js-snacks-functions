c257e66a15b753b2763a4e9f16440ef15d4e1799
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-02 13:02:37 +0200

    risoluzione: - 04-conta-vocali - 05-bonus

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function countVowels(stringa) {
+    const vocali = "aeiou";
+    let count = 0;
 
+    for (let i = 0; i < str.length; i++) {
+        if (vocali.includes(stringa[i])) {
+            count++;
+        }
+    }
+
+    return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(countVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,27 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const nome = 'Mario';
 
 
 // Dichiara la funzione qui.
+function saluta(nome) {
+    const ora = new Date().getHours();
+    let saluto;
 
+    if (ora <= 13) {
+        saluto = "Buongiorno";
+    } else if (ora <= 17) {
+        saluto = "Buon pomeriggio";
+    } else {
+        saluto = "Buonasera";
+    }
+
+    return saluto + " " + nome + ".";
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(saluta(nome));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
